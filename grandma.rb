cab4584f698725e5bcef791c73741fc93411f1d6
Tania Aparicio <tania.e.aparicio@gmail.com>
2017-10-14 21:42:55 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -15,8 +15,10 @@
 def speak_to_grandma(phrase)
   if phrase != phrase.upcase
     return "HUH?! SPEAK UP, SONNY!"
+
   elsif phrase == phrase.upcase
     return "NO, NOT SINCE 1938!"
+
   else phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
   end
